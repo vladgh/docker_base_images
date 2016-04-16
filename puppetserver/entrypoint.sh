6f142fa08fac47e0c8b42452d958148cbee93d0b
Vlad <vlad@ghn.me>
2016-04-16 17:43:37 -0500

    Add aws-sdk gem and an optional autosigning script
    If there is an executable file at `/etc/puppetlabs/puppet/ssl/ca/csr_sign` it
    will be used as the autosigning script

--- puppetserver/entrypoint.sh
+++ puppetserver/entrypoint.sh
@@ -8,9 +8,15 @@ IFS=$'\n\t'
 
 # VARs
 SSLDIR='/etc/puppetlabs/puppet/ssl'
+CSR_SIGN="${SSLDIR}/ca/csr_sign"
 
 # Fix SSL directory ownership
 mkdir -p "$SSLDIR"
 chown -R puppet:puppet "$SSLDIR"
+
+# Configure puppet to use a certificate autosign script (if it exists)
+if [[ -x "$CSR_SIGN" ]]; then
+  puppet config set autosign "$CSR_SIGN" --section master
+fi
 
 exec "$@"
