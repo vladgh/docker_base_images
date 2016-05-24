3ab1508d262ca5c10b90fb4664a93ca592361662
Vlad <vlad@ghn.me>
2016-05-24 15:50:58 -0500

    Deploy R10K if a valid config file is present

--- puppetserver/entrypoint.sh
+++ puppetserver/entrypoint.sh
@@ -9,6 +9,7 @@ IFS=$'\n\t'
 # VARs
 SSLDIR='/etc/puppetlabs/puppet/ssl'
 CSR_SIGN='/etc/puppetlabs/csr/sign'
+R10K_DIR='/etc/puppetlabs/r10k'
 
 # Fix SSL directory ownership
 mkdir -p "$SSLDIR"
@@ -17,6 +18,11 @@ chown -R puppet:puppet "$SSLDIR"
 # Configure puppet to use a certificate autosign script (if it exists)
 if [[ -x "$CSR_SIGN" ]]; then
   puppet config set autosign "$CSR_SIGN" --section master
+fi
+
+# Deploy R10K environments (if a valid configuration file exists)
+if [[ -d "$R10K_DIR" ]] && [[ -s "${R10K_DIR}/r10k.yaml" ]]; then
+  r10k deploy environment --puppetfile --verbose
 fi
 
 exec "$@"
