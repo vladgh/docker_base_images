92192be0c021658bd879b3bd11f49bbcef1a8a59
Vlad <vlad@ghn.me>
2016-07-12 15:16:04 -0500

    Improve s3sync image
    Push changes with `--delete` on all events

--- s3sync/entrypoint.sh
+++ s3sync/entrypoint.sh
@@ -13,29 +13,40 @@ fi
 EVENTS=${EVENTS:-'CREATE,DELETE,MODIFY,MOVE,MOVED_FROM,MOVED_TO'}
 WATCHDIR=${WATCHDIR:-/watch}
 
-# Ensure watched directory exists
-mkdir -p "$WATCHDIR"
+sync_down(){
+  aws s3 sync --delete "$S3PATH" "$WATCHDIR" || true
+}
 
-# Initial download
-aws s3 sync --delete "$S3PATH" "$WATCHDIR" || true
+sync_up(){
+  aws s3 sync --delete "$WATCHDIR" "$S3PATH" || true
+}
 
-sync() {
+sync(){
   case "$@" in
     DELETE* | MOVED_FROM*)
-      aws s3 sync --delete "$WATCHDIR" "$S3PATH" || true
+      sync_up
       ;;
     *)
-      aws s3 sync "$WATCHDIR" "$S3PATH" || true
-      aws s3 sync "$S3PATH" "$WATCHDIR" || true
+      sync_up
       ;;
   esac
 }
 
-watch() {
+watch(){
   inotifywait -e "$EVENTS" -m -r --format '%:e %f' "$WATCHDIR"
 }
 
-# Watch
-watch | (
-  while true; do read -r -t 1 LINE && sync "$LINE"; unset LINE; done
-)
+main(){
+  # Ensure watched directory exists
+  mkdir -p "$WATCHDIR"
+
+  # Initial download
+  sync_down
+
+  # Watch
+  watch | (
+    while true; do read -r -t 1 EVENT && sync "$EVENT"; unset EVENT; done
+  )
+}
+
+main
