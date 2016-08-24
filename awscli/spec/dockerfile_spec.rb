f19edc823580e2060a7a65b1499797f3843d5445
Vlad <vlad@ghn.me>
2016-08-23 19:08:24 -0500

    Add more tests

---
+++ awscli/spec/dockerfile_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+CURRENT_DIRECTORY = File.dirname(File.dirname(__FILE__))
+
+describe 'Dockerfile' do
+  include_context 'with a perpetual docker container'
+
+  it 'uses the correct OS' do
+    expect(os[:family]).to eq('alpine')
+  end
+
+  describe package('bash') do
+    it { is_expected.to be_installed }
+  end
+
+  describe package('git') do
+    it { is_expected.to be_installed }
+  end
+
+  describe command('aws --version') do
+    its(:stderr) { is_expected.to contain('aws-cli') }
+    its(:exit_status) { is_expected.to eq 0 }
+  end
+
+  describe command('git version') do
+    its(:stdout) { is_expected.to contain('git') }
+    its(:exit_status) { is_expected.to eq 0 }
+  end
+end
