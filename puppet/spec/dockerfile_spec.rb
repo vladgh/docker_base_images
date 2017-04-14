57928dff4a9f122d6f09055f1ad93711843b7c44
Vlad <vladgh@users.noreply.github.com>
2017-04-14 11:56:36 -0500

    Add Tini to the Puppet Agent image

--- puppet/spec/dockerfile_spec.rb
+++ puppet/spec/dockerfile_spec.rb
@@ -19,4 +19,14 @@ describe 'Dockerfile' do
     its(:stdout) { is_expected.to contain('.') }
     its(:exit_status) { is_expected.to eq 0 }
   end
+
+  describe file('/sbin/tini') do
+    it { is_expected.to exist }
+    it { is_expected.to be_executable }
+  end
+
+  describe command('/sbin/tini --version') do
+    its(:stdout) { is_expected.to contain('tini') }
+    its(:exit_status) { is_expected.to eq 0 }
+  end
 end
