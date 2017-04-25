b7f58c8c352b3981806cb21155fc73a21759a2ac
Vlad <vlad@ghn.me>
2017-04-25 12:15:27 -0500

    Improve build hooks

--- shlib/hooks.sh
+++ shlib/hooks.sh
@@ -1,24 +1,32 @@
 #!/usr/bin/env bash
 
-# NOTE:
-# By default Docker HUB clones the repository with `--depth=1`. You should
-# deepen the history of the original shallow repository (git describe needs the
-# latest tags to work). Setting this to '50' should incorporate the latest tags,
-# while still keeping the size rather small.
-#     $ git pull --depth=50
+# Bash strict mode
+set -euo pipefail
+IFS=$'\n\t'
 
 # VARs
 GIT_TAG="$(git describe --always --tags)"
 
 # Build hook
 run_build_hook(){
+  deepen_git_repo
   docker_build_image
 }
 
 # Post-Push hook
 run_post_push_hook(){
   tag_semantic_versions
-  notify_webhook
+  notify_webhook "${@:-}"
+}
+
+# Deepen repository history
+# By default Docker HUB clones the repository with `--depth=1`. You should
+# deepen the history of the original shallow repository (git describe needs the
+# latest tags to work). Setting this to '50' should incorporate the latest tags,
+# while still keeping the size rather small.
+deepen_git_repo(){
+  echo 'Deepen repository history'
+  git pull --depth=50
 }
 
 # Build the image with the specified arguments
@@ -33,6 +41,8 @@ docker_build_image(){
 }
 
 tag_semantic_versions(){
+  local IFS=$' ' # required by the version components below
+
   # If tag matches semantic version
   if [[ "$GIT_TAG" != v* ]]; then
     echo "Version (${GIT_TAG}) does not match semantic version; Skipping..."
@@ -65,9 +75,9 @@ tag_semantic_versions(){
 }
 
 notify_webhook(){
-  if [[ -n "${WEBHOOK:-}" ]]; then
+  local webhook="${1:-}"
+  if [[ -n "${webhook:-}" ]]; then
     echo 'Notify webhook'
-    curl -X POST "$WEBHOOK"
+    curl -X POST "$webhook"
   fi
 }
-
