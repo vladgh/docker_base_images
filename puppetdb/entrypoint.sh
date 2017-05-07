95984735b5d5a167039afd6318b7a8428b173838
Vlad <vlad@ghn.me>
2017-05-07 08:56:21 -0500

    Improve PuppetDB entrypoint
    Allow database connection to be specified through environment variables
    at runtime

--- puppetdb/entrypoint.sh
+++ puppetdb/entrypoint.sh
@@ -6,23 +6,50 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# Generate Puppet Certificates
-if [[ ! -s "$(puppet config print hostcert)" ]] && \
-  [[ ! -s "$(puppet config print hostprivkey)" ]] && \
-  [[ ! -s "$(puppet config print localcacert)" ]]
-then
-  echo 'Waiting for Puppet Server...'
-  while ! nc -z puppet 8140; do
-    sleep 1
-  done
-  puppet agent --verbose --onetime --no-daemonize --waitforcert 120
-else
-  echo 'Puppet certificates already configured'
-fi
+# VARs
+PUPPETDB_HOST="${PUPPETDB_HOST:-postgres}"
+PUPPETDB_PORT="${PUPPETDB_PORT:-5432}"
+PUPPETDB_NAME="${PUPPETDB_NAME:-puppetdb}"
+PUPPETDB_USER="${PUPPETDB_USER:-puppetdb}"
+PUPPETDB_PASS="${PUPPETDB_PASS:-puppetdb}"
 
-# Generate PuppetDB certificates
-# /etc/puppetlabs/puppetdb/ssl is automatically populated from Puppet certificates
-# and doesn't need a separate volume.
-puppetdb ssl-setup -f
+# PATHs
+PUPPETDB_CONF='/etc/puppetlabs/puppetdb/conf.d/database.ini'
+PUPPETDB_HTTP='/etc/puppetlabs/puppetdb/conf.d/jetty.ini'
+
+# Check if already initialized
+if [[ -f /etc/puppetlabs/initialized ]]; then
+  echo "PuppetDB already initialized"
+else
+  # Change database connection settings
+  sed -i.bak "s@# subname.*@subname = //$PUPPETDB_HOST:$PUPPETDB_PORT/$PUPPETDB_NAME@gi" "$PUPPETDB_CONF"
+  sed -i "s@# username.*@username = $PUPPETDB_USER@gi" "$PUPPETDB_CONF"
+  sed -i "s@# password.*@password = $PUPPETDB_PASS@gi" "$PUPPETDB_CONF"
+
+  # Other settings
+  sed -i.bak "s@# host.*@host = 0\.0\.0\.0@gi" "$PUPPETDB_HTTP"
+
+  # Generate Puppet Certificates
+  if [[ ! -s "$(puppet config print hostcert)" ]] && \
+    [[ ! -s "$(puppet config print hostprivkey)" ]] && \
+    [[ ! -s "$(puppet config print localcacert)" ]]
+  then
+    echo 'Waiting for Puppet Server...'
+    while ! nc -z puppet 8140; do
+      sleep 1
+    done
+    puppet agent --verbose --onetime --no-daemonize --waitforcert 120
+  else
+    echo 'Puppet certificates already configured'
+  fi
+
+  # Generate PuppetDB certificates
+  # /etc/puppetlabs/puppetdb/ssl is automatically populated from Puppet certificates
+  # and doesn't need a separate volume.
+  puppetdb ssl-setup -f
+
+  # Mark as initialized
+  touch /etc/puppetlabs/initialized
+fi
 
 exec "$@"
