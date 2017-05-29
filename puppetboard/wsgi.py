22eea5442d983c1e50453b3670c89db8aaf84269
Vlad <vlad@ghn.me>
2017-05-29 17:03:16 -0500

    Add logging to Puppet Board that matches Gunicorn

--- puppetboard/wsgi.py
+++ puppetboard/wsgi.py
@@ -3,11 +3,14 @@ import os
 import socket
 import time
 import httplib
+import logging
+
+# Set-up logging to match gunicorn's
+logging.basicConfig(format='%(asctime)s [%(process)d] [%(levelname)s] %(message)s', datefmt='[%Y-%m-%d %H:%M:%S %z]', level=logging.INFO)
 
 # Puppetboard has the irritating hard requirement that puppetdb
 # must be running and listening before it starts, and will otherwise
 # crash. This makes starting services difficult.
-
 conn = httplib.HTTPConnection(os.getenv('PUPPETDB_HOST', 'puppetdb'), int(os.getenv('PUPPETDB_PORT', '8080')), timeout=10)
 
 while True:
@@ -16,6 +19,7 @@ while True:
         response = conn.getresponse()
         status = response.status
     except (socket.error, httplib.ResponseNotReady, httplib.ImproperConnectionState, httplib.HTTPException):
+        logging.info('Waiting for PuppetDB')
         time.sleep(5)
     else:
         if status == 302:
