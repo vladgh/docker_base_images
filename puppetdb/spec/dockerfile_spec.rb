f604942ed6a492a7c1969f4bbe1eaea577079247
Vlad <vlad@ghn.me>
2018-03-25 19:05:19 -0500

    Nest Puppet repositories
    Closes #59

--- puppetdb/spec/dockerfile_spec.rb
+++ puppetdb/spec/dockerfile_spec.rb
@@ -12,37 +12,12 @@ describe 'Dockerfile' do
     expect(@image.json["Config"]["Labels"].has_key?("maintainer"))
   end
 
-  it 'uses the correct version of Ubuntu' do
-    os_version = command('cat /etc/lsb-release').stdout
-    expect(os_version).to include('16.04')
-    expect(os_version).to include('Ubuntu')
-  end
-
-  describe package('puppet-agent') do
-    it { is_expected.to be_installed }
-  end
-
-  describe command('puppet --version') do
-    its(:stdout) { is_expected.to contain('.') }
-    its(:exit_status) { is_expected.to eq 0 }
-  end
-
   describe package('puppetdb') do
     it { is_expected.to be_installed }
   end
 
   describe command('puppetdb --version') do
     its(:stdout) { is_expected.to contain('puppetdb') }
-    its(:exit_status) { is_expected.to eq 0 }
-  end
-
-  describe file('/sbin/tini') do
-    it { is_expected.to exist }
-    it { is_expected.to be_executable }
-  end
-
-  describe command('/sbin/tini --version') do
-    its(:stdout) { is_expected.to contain('tini') }
     its(:exit_status) { is_expected.to eq 0 }
   end
 
