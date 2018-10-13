224506ecb15fe468e7eddeeeb125875bd1b01b71
Vlad <vlad@ghn.me>
2018-10-13 11:05:53 -0500

    Improve release script Closes #73

--- scripts/release.sh
+++ scripts/release.sh
@@ -11,6 +11,8 @@ IFS=$'\n\t'
 # VARs
 GIT_TAG="$(git describe --always --tags)"
 WRITE_CHANGELOG="${WRITE_CHANGELOG:-false}"
+REQUIRE_PULL_REQUEST="${REQUIRE_PULL_REQUEST:-false}"
+WAIT_FOR_CI_SUCCESS="${WAIT_FOR_CI_SUCCESS:-false}"
 BUG_LABELS="${BUG_LABELS:-bug}"
 ENHANCEMENT_LABELS="${ENHANCEMENT_LABELS:-enhancement}"
 
@@ -105,13 +107,40 @@ main(){
 
   RELEASE="v${MAJOR}.${MINOR}.${PATCH}"
 
+  # Detect branch names
+  INITIAL_BRANCH="${GIT_BRANCH:-$(git symbolic-ref --short HEAD 2>/dev/null)}"
+  if [[ "$REQUIRE_PULL_REQUEST" == 'true' ]]; then
+    RELEASE_BRANCH="release_${RELEASE//./_}"
+  else
+    RELEASE_BRANCH="$INITIAL_BRANCH"
+  fi
+
   if [[ "$WRITE_CHANGELOG" == 'true' ]]; then
     generate_log
 
     git diff --quiet HEAD || (
+
+      if [[ "$REQUIRE_PULL_REQUEST" == 'true' ]]; then
+        echo 'Create a new release branch'
+        git checkout -b "$RELEASE_BRANCH"
+      fi
+
       echo 'Commit CHANGELOG'
       git add CHANGELOG.md
       git commit --gpg-sign --message "Update change log for ${RELEASE}" CHANGELOG.md
+
+      if [[ "$WAIT_FOR_CI_SUCCESS" == 'true' ]]; then
+        echo 'Waiting for CI to finish'
+        while [[ $(hub ci-status "$RELEASE_BRANCH") != "success" ]]; do
+          sleep 5
+        done
+      fi
+
+      if [[ "$REQUIRE_PULL_REQUEST" == 'true' ]]; then
+        echo 'Merge release branch'
+        git checkout "$INITIAL_BRANCH"
+        git merge --gpg-sign --no-ff --message "Release ${RELEASE}" "$RELEASE_BRANCH"
+      fi
     )
   fi
 
